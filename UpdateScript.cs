6124930a5007d87167eb7988a86cc95e5d7b4694
Eyal Zusiman <eyal.zusiman@gmail.com>
2021-11-02 11:32:35 +0200

    removed debug code from last commit

--- UpdateScript.cs
+++ UpdateScript.cs
@@ -12,12 +12,6 @@ public class UpdateScript : Node
 	
 	public override void _Ready()
 	{
-		GD.Print(DamageCalculator.DamageToColor(0)*255);
-		GD.Print(DamageCalculator.DamageToColor(10)*255);
-		GD.Print(DamageCalculator.DamageToColor(50)*255);
-		GD.Print(DamageCalculator.DamageToColor(100)*255);
-		GD.Print(DamageCalculator.DamageToColor(150)*255);
-		GD.Print(DamageCalculator.DamageToColor(300)*255);
 		//IterUtils.Flatten({1, 4, 5, 6}, {9, 6, 7, 4}, {"hag", "esah", "agsag"}).Debug();
 		//(new int[] {1, 5, 6, 4, 0, 9, 15, 1251, 125, 87}).Indexed().Cast<object>().Debug();
 	}
