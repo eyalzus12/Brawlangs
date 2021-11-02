6f3797a8bd8fdff572b1bc663e945e11908be0a8
Eyal Zusiman <eyal.zusiman@gmail.com>
2021-11-02 15:04:52 +0200

    final result for damage calculator, that now goes to black for 300 using my new and better understanding of the system

--- Scripts/DamageCalculator.cs
+++ Scripts/DamageCalculator.cs
@@ -4,10 +4,6 @@ using System.Linq;
 
 public class DamageCalculator
 {
-	//public readonly static float[] MULTIPLIERS = {0.02f, 0.008f, 0.012f, 0.005f, 0.004f, 0.0052f};
-	//public readonly static float[] MULTIPLIERS = {0.02f, 0.008f, 0.012f, 0.0066f, 0.0066f, 0.0066f};
-	//public readonly static float[] MULTIPLIERS = {1f, 0.4f, 0.6f, 0.2f, 0.4f, 0.4f};
-	//public readonly static int[] INDEXES = {2, 1, 1, 0, 0, 0};
 	
 	public readonly static float[][] VALUES = {
 		new float[]{1f},
@@ -24,53 +20,20 @@ public class DamageCalculator
 	
 	public static Color DamageToColor(float damage)
 	{
-		Color color = default;
-		color.a = 1f;
+		var color = new Color(1f, 1f, 1f, 1f);
 		
-		foreach(var i in ORDER)
+		foreach(var i in ORDER) for(int j = 0; j < VALUES[i].Length && damage > 0f; ++j, damage -= jump)
 		{
-			for(int j = 0; j < VALUES[i].Length && damage > 0f; ++j, damage -= jump)
+			var reduce = VALUES[i][j] * Math.Min(damage/jump, 1f);
+			switch(i)
 			{
-				var addition = VALUES[i][j] * Math.Min(damage/jump, 1f);
-				switch(i)
-				{
-					case 0:
-						color.r += addition;
-						 break;
-					case 1:
-						color.g += addition;
-						break;
-					case 2:
-						color.b += addition;
-						break;
-					default:
-						GD.Print("Improper value for color index {INDEXES[i]} at index {i}");
-						break;
-				}
+				case 0: color.r -= reduce; break;
+				case 1: color.g -= reduce; break;
+				case 2: color.b -= reduce; break;
+				default: GD.Print("Improper color index {ORDER}"); break;
 			}
 		}
 		
-		/*var length = INDEXES.Length;
-		for(int i = 0; i < length && damage > 0f; ++i, damage -= jump)
-		{
-			var addition = MULTIPLIERS[i] * Math.Min(damage/jump, 1f);
-			switch(INDEXES[i])
-			{
-				case 0:
-					color.r += addition;
-					 break;
-				case 1:
-					color.g += addition;
-					break;
-				case 2:
-					color.b += addition;
-					break;
-				default:
-					GD.Print("Improper value for color index {INDEXES[i]} at index {i}");
-					break;
-			}
-		}*/
-		
-		return color.Inverted();
+		return color;
 	}
 }
