1825ae257c1a695aca42e8f0d0615b7349dd8afb
Eyal Zusiman <eyal.zusiman@gmail.com>
2021-11-02 15:36:49 +0200

    pro tip: compile before commiting

--- Scripts/DamageCalculator.cs
+++ Scripts/DamageCalculator.cs
@@ -10,30 +10,16 @@ public class DamageCalculator
 	
 	public const float MAX_DAMAGE = 300f;
 	
-	public static int length = VALUES.Aggregate(0,(a,v)=>a+v.Length);
+	public static int length = VALUES.Length;
 	public static float jump = MAX_DAMAGE/length;
 	
 	public static Color DamageToColor(float damage)
 	{
 		var color = new Color(1f, 1f, 1f, 1f);
 		
-		/*foreach(var i in ORDER) for(int j = 0; j < VALUES[i].Length && damage > 0f; ++j, damage -= jump)
-		{
-			var reduce = VALUES[i][j] * Math.Min(damage/jump, 1f);
-			switch(i)
-			{
-				case 0: color.r -= reduce; break;
-				case 1: color.g -= reduce; break;
-				case 2: color.b -= reduce; break;
-				default: GD.Print("Improper color index {ORDER}"); break;
-			}
-			
-			return color.Inverted();
-		}*/
-		
 		for(int i = 0; i < length && damage > 0f; ++i, damage -= jump)
 		{
-			var reduce = MULTIPLIERS[i] * Math.Min(damage/jump, 1f);
+			var reduce = VALUES[i] * Math.Min(damage/jump, 1f);
 			switch(ORDER[i])
 			{
 				case 0: color.r -= reduce; break;
